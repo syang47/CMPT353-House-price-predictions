344dcc73e8146da72017b3a058853565c4f358cd
syang47 <38193793+syang47@users.noreply.github.com>
2021-07-29 02:15:10 -0700

    added num_amenities dictionary and cleaned up more osm data

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ def clean_amenities_data(amenities_data, amenities_required):
     #find unique amenities and the number of them to choose which are important for a traveller
     # print(amenities_data['amenity'].value_counts())
 
-    
+
     #adapted from : https://www.kite.com/python/answers/how-to-filter-a-pandas-dataframe-with-a-list-by-%60in%60-or-%60not-in%60-in-python
     bool_series = amenities_data.amenity.isin(amenities_required)
     filtered_amenities_df = amenities_data[bool_series]
+    filtered_amenities_df = filtered_amenities_df.drop(['timestamp','tags'], axis=1).dropna() # dropping unnecessary columns, and filter out NA values
     filtered_amenities_df.reset_index(inplace=True, drop=True)
 
     return filtered_amenities_df
@@ -34,6 +35,19 @@ def haversine_distance(lon1, lat1, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r * 1000
 
+def haversine_distance2(df, lon2, lat2):
+    # convert decimal degrees to radians 
+    lon1=np.radians(df['lon'])
+    lat1=np.radians(df['lat'])
+    lon2=np.radians(lon2)
+    lat2=np.radians(lat2)
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = (dlat/2).apply(sin)**2 + (lat1).apply(sin) * cos(lat2) * (dlon/2).apply(sin)**2
+    c = 2 * ((a).apply(sqrt).apply(asin)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r * 1000
 
 def clean_listings_data(listings_data):
     #keep only the columns we need
@@ -42,28 +56,35 @@ def clean_listings_data(listings_data):
     return listings_data
 
 
+# #TODO: return a dictionary with number of amenities in a 1km radius of this lat and lon
+def num_amenities(lat, lon, amenities_data_clean):
+    distance = haversine_distance2(amenities_data_clean, lon, lat)
+    amenities_data_clean['distance'] = distance
+    data_withinR = amenities_data_clean.loc[amenities_data_clean['distance'] < 1000].reset_index(drop=True)
+    amenities_series = data_withinR.pivot_table(columns = ['amenity'], aggfunc='size')  # Counts # of amenities, type=pd.series
+    amenities_dict = amenities_series.to_dict()# converts series to dict
+    return amenities_dict
+#     amenities_dict = dict.fromkeys(amenities_required,0)
 
-#TODO: return a dictionary with number of amenities in a 1km radius of this lat and lon
-def num_amenities(lat, lon, amenities_data_clean, amenities_required):
-    amenities_dict = dict.fromkeys(amenities_required,0)
     
-
 
 def main():
+
     #Read Data
     listings_data, amenities_data = read_data()
-    
-    # Change amenities here 
-    amenities_required = ['restaurent', 'fast_food', 'cafe','bank','atm','pharmacy','bicycle_rental','fuel','pub','bar','car_sharing','car_rental','clinic','doctors','hospital','ice_cream','fountain','theatre','police','bus_station']
+
+    # Change amenities here (updated the "restaurant" typo)
+    amenities_required = ['restaurant', 'fast_food', 'cafe','bank','atm','pharmacy','bicycle_rental','fuel','pub','bar','car_sharing','car_rental','clinic','doctors','hospital','ice_cream','fountain','theatre','police','bus_station']
 
     #Data Cleaning
     amenities_data_clean = clean_amenities_data(amenities_data, amenities_required)
     listings_data_clean = clean_listings_data(listings_data)
 
-    #TODO: Add a column to listings dataset:  each element with a dictionary of number of amenities in a 1km radius of a listing.
-
-
-
+    #Return a dict of amenities:
+    lat_input = 49.225164  # sample lat input
+    lon_input = -123.003742  # sample lon input
+    amen_dict = num_amenities(lat_input, lon_input, amenities_data_clean)
+    
 
 if __name__ == "__main__":
     main()
