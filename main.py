65bddae6ab42ab1ff99bb8e25976d3da024e993c
Dipansh Arora <dipansharora@Dipanshs-MacBook-Pro.local>
2021-07-30 15:53:03 -0700

    added num_amenities_nearby column in listings dataset

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def clean_amenities_data(amenities_data, amenities_required):
     #find unique amenities and the number of them to choose which are important for a traveller
     # print(amenities_data['amenity'].value_counts())
 
-
     #adapted from : https://www.kite.com/python/answers/how-to-filter-a-pandas-dataframe-with-a-list-by-%60in%60-or-%60not-in%60-in-python
     bool_series = amenities_data.amenity.isin(amenities_required)
     filtered_amenities_df = amenities_data[bool_series]
@@ -24,18 +23,7 @@ def clean_amenities_data(amenities_data, amenities_required):
 
 
 #reference: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-def haversine_distance(lon1, lat1, lon2, lat2):
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r * 1000
-
-def haversine_distance2(df, lon2, lat2):
+def haversine_distance(df, lon2, lat2):
     # convert decimal degrees to radians 
     lon1=np.radians(df['lon'])
     lat1=np.radians(df['lat'])
@@ -56,16 +44,14 @@ def clean_listings_data(listings_data):
     return listings_data
 
 
-# #TODO: return a dictionary with number of amenities in a 1km radius of this lat and lon
+# return a dictionary with number of amenities in a 1km radius of this lat and lon
 def num_amenities(lat, lon, amenities_data_clean):
-    distance = haversine_distance2(amenities_data_clean, lon, lat)
+    distance = haversine_distance(amenities_data_clean, lon, lat)
     amenities_data_clean['distance'] = distance
     data_withinR = amenities_data_clean.loc[amenities_data_clean['distance'] < 1000].reset_index(drop=True)
     amenities_series = data_withinR.pivot_table(columns = ['amenity'], aggfunc='size')  # Counts # of amenities, type=pd.series
     amenities_dict = amenities_series.to_dict()# converts series to dict
     return amenities_dict
-#     amenities_dict = dict.fromkeys(amenities_required,0)
-
     
 
 def main():
@@ -80,11 +66,9 @@ def main():
     amenities_data_clean = clean_amenities_data(amenities_data, amenities_required)
     listings_data_clean = clean_listings_data(listings_data)
 
-    #Return a dict of amenities:
-    lat_input = 49.225164  # sample lat input
-    lon_input = -123.003742  # sample lon input
-    amen_dict = num_amenities(lat_input, lon_input, amenities_data_clean)
-    
+    #add a column for number of amenities nearby to each listing
+    listings_data_clean['num_amenities_nearby'] = listings_data_clean.apply(lambda x: num_amenities(x['latitude'], x['longitude'], amenities_data_clean), axis = 1)
+    print(listings_data_clean)
 
 if __name__ == "__main__":
     main()
