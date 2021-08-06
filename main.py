3b2079a67d502851930b8a4ea58365a43fde6303
syang47 <38193793+syang47@users.noreply.github.com>
2021-08-05 20:36:34 -0700

    added listings's data cleaning, and rough idea on sorting and associating with the amenities' data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 from math import radians, cos, sin, asin, sqrt
 
 def read_data():
@@ -13,7 +14,6 @@ def clean_amenities_data(amenities_data, amenities_required):
     #find unique amenities and the number of them to choose which are important for a traveller
     # print(amenities_data['amenity'].value_counts())
 
-
     #adapted from : https://www.kite.com/python/answers/how-to-filter-a-pandas-dataframe-with-a-list-by-%60in%60-or-%60not-in%60-in-python
     bool_series = amenities_data.amenity.isin(amenities_required)
     filtered_amenities_df = amenities_data[bool_series]
@@ -24,18 +24,7 @@ def clean_amenities_data(amenities_data, amenities_required):
 
 
 #reference: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
-def haversine_distance(lon1, lat1, lon2, lat2):
-    # convert decimal degrees to radians 
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    return c * r * 1000
-
-def haversine_distance2(df, lon2, lat2):
+def haversine_distance(df, lon2, lat2):
     # convert decimal degrees to radians 
     lon1=np.radians(df['lon'])
     lat1=np.radians(df['lat'])
@@ -49,24 +38,69 @@ def haversine_distance2(df, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r * 1000
 
-def clean_listings_data(listings_data):
+def clean_listings_data(listings_data, accommodates_input, room_input, price_range_input, exact):
     #keep only the columns we need
-    columns_needed = ['id', 'listing_url', 'name', 'description', 'picture_url', 'latitude', 'longitude', 'property_type', 'accommodates', 'bedrooms', 'beds', 'amenities', 'price']
-    listings_data = listings_data[columns_needed]
+    columns_needed = ['id', 'listing_url', 'name', 'description', 'picture_url', 'latitude', 'longitude', 'property_type', 'accommodates', 'bedrooms', 'beds', 'amenities', 'price',  'review_scores_value']
+    listings_data = listings_data[columns_needed].copy()
+    listings_data['price'] = listings_data['price'].apply(lambda x: x.replace(',','').replace('$','')).astype(float)
+    
+    pd.set_option('mode.chained_assignment', None)
+    # extract price_range from string
+    p_range = [float(s) for s in re.findall('[^-]?\d[\d.,]+', price_range_input)]
+    min_price = p_range[0]
+    max_price = p_range[1]
+    
+    bedrooms=listings_data['bedrooms']
+    accommodates=listings_data['accommodates']
+    if not exact:
+        # find listing data with bedrooms >= room_input and accommodates >= accomodates_input
+        listings_data = listings_data[(bedrooms >= room_input) & (accommodates >= accommodates_input)].reset_index(drop=True)
+        listings_data = listings_data[(listings_data['price'] <= max_price) & (listings_data['price'] >= min_price)].reset_index(drop=True)
+    
+    else:
+        # find listing data with bedrooms == room_input and accommodates == accomodates_input
+        listings_data = listings_data[(bedrooms == room_input) & (accommodates == accommodates_input)].reset_index(drop=True)
+        listings_data = listings_data[(listings_data['price'] <= max_price) & (listings_data['price'] >= min_price)].reset_index(drop=True)
+    if listings_data.empty:
+        print("Cannot find any listings with current filter, please try with other filters.\n")
+        return
+    
+    
+    
     return listings_data
 
+# haven't fixed bugs yet
+def output_by_sort(listing_data):
+    # sort by price in ascending order
+    listings_by_price = listings_data.sort_values(by="price", ascending=True).reset_index(drop=True)
 
-# #TODO: return a dictionary with number of amenities in a 1km radius of this lat and lon
+    # sort by review_scores_values in descending order, drop na scores(?)
+    listings_by_rscore = listings_data.sort_values(['review_scores_value'], ascending=False).dropna().reset_index(drop=True)
+
+    # output lowest_price listing, highest scored listing to another dataframe
+    x = listings_by_rscore.head(1)
+    x["result"] = "Best Scored"
+    cols = list(x.columns)
+    cols = [cols[-1]]+cols[:-1]
+    x = x[cols]
+
+    y = listings_by_price.head(1)
+    y["result"] = "Best Valued"
+    cols = list(y.columns)
+    cols = [cols[-1]]+cols[:-1]
+    y = y[cols]
+    
+    temp_output = pd.concat([x, y], axis=0, ignore_index=True)
+    return temp_output
+
+# return a dictionary with number of amenities in a 1km radius of this lat and lon
 def num_amenities(lat, lon, amenities_data_clean):
-    distance = haversine_distance2(amenities_data_clean, lon, lat)
+    distance = haversine_distance(amenities_data_clean, lon, lat)
     amenities_data_clean['distance'] = distance
     data_withinR = amenities_data_clean.loc[amenities_data_clean['distance'] < 1000].reset_index(drop=True)
     amenities_series = data_withinR.pivot_table(columns = ['amenity'], aggfunc='size')  # Counts # of amenities, type=pd.series
     amenities_dict = amenities_series.to_dict()# converts series to dict
     return amenities_dict
-#     amenities_dict = dict.fromkeys(amenities_required,0)
-
-    
 
 def main():
 
@@ -76,14 +110,55 @@ def main():
     # Change amenities here (updated the "restaurant" typo)
     amenities_required = ['restaurant', 'fast_food', 'cafe','bank','atm','pharmacy','bicycle_rental','fuel','pub','bar','car_sharing','car_rental','clinic','doctors','hospital','ice_cream','fountain','theatre','police','bus_station']
 
+    # User Input
+    ## get number of bedrooms and number of accommodates from user
+    room_input = 3
+    accommodates_input = 3
+    price_range_input= "16-10000"
+    exact = True
+
     #Data Cleaning
     amenities_data_clean = clean_amenities_data(amenities_data, amenities_required)
-    listings_data_clean = clean_listings_data(listings_data)
+    listings_data_clean = clean_listings_data(listings_data, accommodates_input, room_input, price_range_input, exact)
 
-    #Return a dict of amenities:
-    lat_input = 49.225164  # sample lat input
-    lon_input = -123.003742  # sample lon input
-    amen_dict = num_amenities(lat_input, lon_input, amenities_data_clean)
+    #add a column for number of amenities nearby to each listing
+    listings_data_clean['num_amenities_nearby'] = listings_data_clean.apply(lambda x: num_amenities(x['latitude'], x['longitude'], amenities_data_clean), axis = 1)
+
+
+    # sort by price in ascending order
+    listings_by_price = listings_data_clean.sort_values(by="price", ascending=True).reset_index(drop=True)
+
+    # sort by review_scores_values in descending order, drop na scores(?)
+    listings_by_rscore = listings_data_clean.sort_values(['review_scores_value'], ascending=False).dropna().reset_index(drop=True)
+
+    # output lowest_price listing, highest scored listing to another dataframe
+    x = listings_by_rscore.head(1)
+    x["result"] = "Best Scored"
+    cols = list(x.columns)
+    cols = [cols[-1]]+cols[:-1]
+    x = x[cols]
+
+    y = listings_by_price.head(1)
+    y["result"] = "Best Valued"
+    cols = list(y.columns)
+    cols = [cols[-1]]+cols[:-1]
+    y = y[cols]
+
+    sorted_output = pd.concat([x, y], axis=0, ignore_index=True)
+    print(sorted_output)
+    # temp_output.shape
+    # temp_output
+
+    # display amenities distance/types around the selected listing 
+    # for the first option example:
+    amen_output = amenities_data_clean.copy()
+    lon = sorted_output.iloc[0,7] # lon from utput listing 
+    lat = sorted_output.iloc[0,6] # lat
+
+    distance = haversine_distance(amen_output, lon , lat)
+    amen_output['distance'] = distance
+    data_withinR = amen_output.loc[amenities_data_clean['distance'] < 1000].reset_index(drop=True)
+    print(data_withinR)
     
 
 if __name__ == "__main__":
