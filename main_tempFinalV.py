e03cfbdc2d8d7b1c690c61378b586d3323eade6e
syang47 <38193793+syang47@users.noreply.github.com>
2021-08-09 18:39:10 -0700

    cleaned and addeded sys, tested exact == true

--- main_tempFinalV.py
+++ main_tempFinalV.py
@@ -8,19 +8,20 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import kneighbors_graph
 from sklearn.ensemble import GradientBoostingRegressor
 import re
+import sys
 
-def read_data():
+# returns input datasets 
+def read_data(in_directory):
     listings_data = pd.read_csv('listings.csv.gz')
     amenities_data = pd.read_json('amenities-vancouver.json.gz', lines=True)
-    user_input1 = pd.read_csv('input1.txt', sep=':\s', names=['Preference','Preference_Data'], engine='python')
+    user_input1 = pd.read_csv(in_directory, sep=':\s', names=['Preference','Preference_Data'], engine='python')
     
     return listings_data, amenities_data, user_input1
 
-
+# returns cleaned amenities df
 def clean_amenities_data(amenities_data, amenities_required):
 
-    #find unique amenities and the number of them to choose which are important for a traveller
-    # print(amenities_data['amenity'].value_counts())
+    # find unique amenities and the number of them to choose which are important for a traveller
 
     #adapted from : https://www.kite.com/python/answers/how-to-filter-a-pandas-dataframe-with-a-list-by-%60in%60-or-%60not-in%60-in-python
     bool_series = amenities_data.amenity.isin(amenities_required)
@@ -30,8 +31,8 @@ def clean_amenities_data(amenities_data, amenities_required):
 
     return filtered_amenities_df
 
-
-#reference: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
+# returns the difference between two points
+# reference: https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
 def haversine_distance(df, lon2, lat2):
     # convert decimal degrees to radians 
     lon1=np.radians(df['lon'])
@@ -46,8 +47,7 @@ def haversine_distance(df, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r * 1000
 
-# 
-
+# returns cleaned data listings
 def clean_listings_data(listings_data, accommodates_input, room_input, price_range_input, exact, amenities_data_clean):
     #keep only the columns we need
     columns_needed = ['id', 'listing_url', 'name', 'description', 'picture_url', 'latitude', 'longitude', 'property_type', 'accommodates', 'bedrooms', 'beds', 'amenities', 'price',  'review_scores_value']
@@ -97,9 +97,9 @@ def num_amenities(lat, lon, amenities_data_clean):
     data_withinR = amenities_data_clean.loc[amenities_data_clean['distance'] < 1000].reset_index(drop=True)
     amenities_series = data_withinR.pivot_table(columns = ['amenity'], aggfunc='size')  # Counts # of amenities, type=pd.series
     amenities_dict = amenities_series.to_dict()# converts series to dict
-    return amenities_dict
-    
+    return amenities_dict    
 
+# returns calculated amenity score
 def ameneties_score(my_dict):
     
     num_different_amenities = len(my_dict)
@@ -112,32 +112,25 @@ def ameneties_score(my_dict):
             score+= my_dict[key]
     return score
 
-def find_best_listing():
-    #Read Data
-    listings_data, amenities_data = read_data()
+# returns cleaned amenities df and listings df sorted by amenity score
+def find_best_listing(listings_data, amenities_data, user_input1):
 
-    # Change amenities here (updated the "restaurant" typo)
+    # handle input file
+    accommodates_input, room_input, price_range_input, exact = handle_input(user_input1)
+    
+    # clean OSM amenities data
     amenities_required = ['restaurant', 'fast_food', 'cafe','bank','atm','pharmacy','bicycle_rental','fuel','pub','bar','car_sharing','car_rental','clinic','doctors','hospital','ice_cream','fountain','theatre','police','bus_station']
-
-    # TODO:turn this into user input in the end 
-    num_accomodates = 10
-    num_bedrooms = 3
-    max_price = 300 
-
-    #Data Cleaning
     amenities_data_clean = clean_amenities_data(amenities_data, amenities_required)
-    listings_data_clean = clean_listings_data(listings_data,num_accomodates,num_bedrooms,max_price)
+    
+    # clean AirBnb listings data
+    listings_data_clean = clean_listings_data(listings_data, accommodates_input, room_input, price_range_input, exact, amenities_data_clean)
+    
+    # sort by amenities_score in descending order, drop na scores(?)
+    listings_by_ascore = listings_data_clean.sort_values(['amenities_score'], ascending=False).dropna().reset_index(drop=True)
+    
+    return amenities_data_clean, listings_by_ascore
 
-    #add a column for number of amenities nearby to each listing
-    listings_data_clean['num_amenities_nearby'] = listings_data_clean.apply(lambda x: num_amenities(x['latitude'], x['longitude'], amenities_data_clean), axis = 1)
-
-    #add a column 'amenities_score' based on the number of different amenities nearby
-    listings_data_clean['amenities_score'] = listings_data_clean['num_amenities_nearby'].apply(lambda x : ameneties_score(x))
-
-    #sort based on amenities score
-    listings_data_clean.sort_values(['amenities_score'], ascending = [False], inplace = True)
-    print(listings_data_clean)
-
+# returns cleaned listings data for ML
 def clean_data_ML(listings_data):
     
     columns_needed = ['latitude', 'longitude', 'host_response_time', 'host_response_rate', 'host_acceptance_rate','host_is_superhost','host_listings_count', 'host_total_listings_count', 'host_identity_verified','neighbourhood_cleansed', 'property_type', 'room_type', 'accommodates', 'bedrooms', 'beds', 'amenities', 'price',   'minimum_nights', 'maximum_nights', 'maximum_nights_avg_ntm',  'availability_30', 'availability_60', 'availability_90','availability_365','number_of_reviews', 'review_scores_rating', 'review_scores_accuracy', 'review_scores_cleanliness', 'review_scores_checkin','review_scores_communication','review_scores_location','review_scores_value', 'reviews_per_month' ]
@@ -165,7 +158,7 @@ def clean_data_ML(listings_data):
     listings_data['price'] = listings_data['price'].apply(lambda x: float(x.replace('$','').replace(',','')))
     return listings_data
 
-    
+# returns the scores of different models
 def run_ml(listings_data_clean, amenities_data_clean):
     
     X = listings_data_clean.drop('price',1)
@@ -217,7 +210,7 @@ def run_ml(listings_data_clean, amenities_data_clean):
 
     return knn_sc, knn_A_sc, rf_sc, rf_A_sc, gb_sc, gb_A_sc
 
-# handles user's input textfile 
+# handles returns converted input values from file 
 def handle_input(inputfile):
     for (x) in range(len(inputfile)):
         if inputfile['Preference'].iloc[x].lower() == "accommodates":
@@ -235,67 +228,12 @@ def handle_input(inputfile):
            
     return accommodates_input, room_input, price_range_input, exact
 
-# generates top 3 listings sorted by best review score, best amenity score, and best price
-def generate_output(listings_data_clean):
-    
-    # sort by price in ascending order
-    listings_by_price = listings_data_clean.sort_values(by="price", ascending=True).reset_index(drop=True)
-
-    # sort by review_scores_values in descending order, drop na scores(?)
-    listings_by_rscore = listings_data_clean.sort_values(['review_scores_value'], ascending=False).dropna().reset_index(drop=True)
-
-    # sort by amenities_score in descending order, drop na scores(?)
-    listings_by_ascore = listings_data_clean.sort_values(['amenities_score'], ascending=False).dropna().reset_index(drop=True)
-    
-    # output lowest_price listing, highest scored listing to another dataframe
-    x = listings_by_rscore.head(1)
-    x["result"] = "Best Scored"    
-    cols = list(x.columns)
-    cols = [cols[-1]]+cols[:-1]
-    x = x[cols]
-
-    y = listings_by_price.head(1)
-    y["result"] = "Best Valued"
-    cols = list(y.columns)
-    cols = [cols[-1]]+cols[:-1]
-    y = y[cols]
-
-    z = listings_by_ascore.head(1)
-    z["result"] = "Best Amenity Scored"
-    cols = list(z.columns)
-    cols = [cols[-1]]+cols[:-1]
-    z = z[cols]
-
-    sorted_output = pd.concat([z, x, y], axis=0, ignore_index=True)  
-    return sorted_output
-
-# returns amenities around the top 3 listings
-def find_amenties_around_top_listings(sorted_output, amenities_data_clean, i):
-    amen_output = amenities_data_clean.copy()
-    lon = sorted_output.iloc[i,7] # lon from utput listing 
-    lat = sorted_output.iloc[i,6] # lat
-    distance = haversine_distance(amen_output, lon, lat)
-    amen_output['distance'] = distance
-    data_withinR = amen_output.loc[amen_output['distance'] < 1000].reset_index(drop=True)
-        
-    return data_withinR
-
-def main():
+def main(in_directory):
     # read data
-    listings_data, amenities_data, user_input1 = read_data()
-    
-    # handle input file
-    
-    accommodates_input, room_input, price_range_input, exact = handle_input(user_input1)
-    
-
-    # clean OSM amenities data
-    amenities_required = ['restaurant', 'fast_food', 'cafe','bank','atm','pharmacy','bicycle_rental','fuel','pub','bar','car_sharing','car_rental','clinic','doctors','hospital','ice_cream','fountain','theatre','police','bus_station']
-    amenities_data_clean = clean_amenities_data(amenities_data, amenities_required)
-    print(amenities_data_clean)
-    
-    # clean AirBnb listings data
-    listings_data_clean = clean_listings_data(listings_data, accommodates_input, room_input, price_range_input, exact, amenities_data_clean)
+    listings_data, amenities_data, user_input1 = read_data(in_directory)
+        
+    # sort filtered data by best review score, best amenity score, and best price
+    [amenities_data_clean, sorted_output] = find_best_listing(listings_data, amenities_data, user_input1)
 
     # Perform ML Trials and store output score to df
     [knn_sc, knn_A_sc, rf_sc, rf_A_sc, gb_sc, gb_A_sc] = run_ml(clean_data_ML(listings_data), amenities_data_clean)
@@ -306,38 +244,15 @@ def main():
                          columns=["AirBnb's Listing Info", "AirBnb's Listing Info with Amenity Scores"], 
                          index=['K-Nearest Neighbors','Random Forest','Gradient Boosting'])
     ML_df.index.name = "Regressors Used"
-    print(ML_df)
-
-    # sort filtered data by best review score, best amenity score, and best price
-    sorted_output = generate_output(listings_data_clean)
-
-    # outputting the filtered listings --> top 3 and total
-    TOP3_OUT = sorted_output.to_csv("Top3_Filtered_ABNB_Listings.csv",na_rep='(missing)')
-    TOTAL_OUT = listings_data_clean.to_csv("Total_Filtered_ABNB_Listings.csv",na_rep='(missing)')
     
-    # outputting nearby amenities around the top 3 filtered listing
-    for i in range(len(sorted_output)):
-        if sorted_output['result'].iloc[i] == "Best Amenity Scored":
-            data_withinA = find_amenties_around_top_listings(sorted_output, amenities_data_clean, i)
-            data_withinA.index.name = "Best Amenity Scored"
-            data_withinA.to_csv('Best_Amenity_Scored_Listing_nearbyAmenities.csv',na_rep='(missing)')
-            print("Found the Best Amenity Scored Listing's nearby amenities")
+    # outputting the filtered listings to csv file
+    TOTAL_OUT = sorted_output.to_csv("Total_Filtered_ABNB_Listings.csv",na_rep='(missing)')
+    
+    # outputting the ML prediction results to csv file
+    ML_OUT = ML_df.to_csv("ML_Price_Prediction.csv",na_rep='(missing')
 
-        if sorted_output['result'].iloc[i] == "Best Scored":
-            data_withinS = find_amenties_around_top_listings(sorted_output, amenities_data_clean, i)
-            data_withinS.index.name = "Best Scored"
-            data_withinS.to_csv('Best_Reviewd_Scored_Listing_nearbyAmenities.csv',na_rep='(missing)')
-            print("Found the Best Review Scored Listing's nearby amenities")
-
-        if sorted_output['result'].iloc[i] == "Best Valued":
-            data_withinP = find_amenties_around_top_listings(sorted_output, amenities_data_clean, i)
-            data_withinA.index.name = "Best Valued"
-            data_withinA.to_csv('Best_Valued_Listing_nearbyAmenities.csv',na_rep='(missing)')
-            print("Found the Best Valued Listing's nearby amenities")
-
-    # outputting the ML prediction results
-    ML_OUT = ML_df.to_csv("ML_Price_Prediction.csv",na_rep='(missing)')
 
     
 if __name__ == "__main__":
-    main()
+    in_directory = sys.argv[1]
+    main(in_directory)
